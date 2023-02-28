f19214c44872eb56d30f2dd5f086b05dce333a30
vjiastelin <36763405+vjiastelin@users.noreply.github.com>
2023-03-01 00:43:56 +0500

    Revert tests (#31)
    * add date picker
    
    * add TZ to docker compose var
    add cors rule for all
    change to localhost in api call
    
    * first test added

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, waitFor } from "@testing-library/react";
+import axios from "axios";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("axios"); // or any other library for making API calls
+
+describe("App", () => {
+  it("displays data from the API", async () => {
+    const currentDate = new Date();
+    const today = new Date(currentDate.getFullYear(),currentDate.getMonth(),currentDate.getDate());
+    const startDate = new Date(today.getTime() + (9 * 60 * 60 * 1000));
+    const endDate = new Date(today.getTime() + (10 * 60 * 60 * 1000));
+    const mockData = [{
+      startDate: startDate.toISOString(),
+      endDate: endDate.toISOString(),
+      title: "Meeting",
+      type: "private",
+    }];
+
+    //@ts-ignore
+    axios.get.mockResolvedValueOnce({ data: mockData });
+    const { getByText } = render(<App />);
+    await waitFor(() => expect(getByText("Meeting")).toBeInTheDocument());
+   
+  });
 });
--- server/Program.cs
+++ server/Program.cs
@@ -10,8 +10,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy  =>
                       {
-                          policy.WithOrigins("http://localhost:3000",
-                                              "http://scheduler-client:3000");
+                          policy.WithOrigins("*");
                       });
 });
 
