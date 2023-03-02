821fe4206a04824b6caf12259743d9e9c2894738
skrezz <fordermo@mail.ru>
2023-03-02 22:58:12 +0300

    2 tests done working. GetAppo_WithRightDate() DelAppo_WithRightId()

---
+++ xUnitTests/SchedulingServiceTests.cs
@@ -0,0 +1,51 @@
+using EWS_NetCore_Scheduler.Service;
+using EWS_NetCore_Scheduler.Interfaces;
+using Moq;
+using Castle.Components.DictionaryAdapter.Xml;
+using Microsoft.Exchange.WebServices.Data;
+using System.Net.WebSockets;
+using Microsoft.AspNetCore.Mvc;
+using EWS_NetCore_Scheduler.Models;
+
+namespace xUnitTests
+{
+    public class SchedulingServiceTests
+    {
+        private readonly SchedulingService _shedServTest;
+        private readonly Mock<IEWSActing> _IEWSActingMock = new Mock<IEWSActing>();
+        public SchedulingServiceTests()
+        {
+            _shedServTest =  new SchedulingService(_IEWSActingMock.Object);
+        }
+        [Fact]
+        public void DelAppo_WithRightId()
+        {
+            //arrange
+            var id = Guid.NewGuid();
+            //Act
+            var res= _shedServTest.DelAppo(id.ToString());
+            //assert
+            Assert.Equal("deleted", res);
+        }
+
+        [Fact]
+        public void GetAppo_WithRightDate()
+        {
+            //arrange
+            var startDate = new DateTime();
+            var appo = new Appo[0];          
+            
+            var Jsonappo = new JsonResult(appo);
+            /*FindItemsResults<Item> findItem =_IEWSActingMock.Object.FindAppointments(_IEWSActingMock.Object.CrEwsService());
+
+            findItem.Items.Add(new Appointment(_IEWSActingMock.Object.CrEwsService()));
+            
+            _IEWSActingMock.Setup(x => x.FindAppointments(_IEWSActingMock.Object.CrEwsService())).Returns(findItem);*/
+                
+            //Act
+            var res = _shedServTest.GetAppos(startDate.ToString());
+            //assert
+            Assert.Equal(Jsonappo.Value, res.Value);
+        }
+    }
+}
